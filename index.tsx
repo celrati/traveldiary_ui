3291c800a54ea68a22f9414c24326795554c66f0
celrati <m.charif.achraf@gmail.com>
2021-04-17 00:04:37 +0200

    fix some depe issues

--- index.tsx
+++ index.tsx
@@ -4,14 +4,20 @@ import App from './App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
+import { Provider as AntProvider } from '@ant-design/react-native';
 
 
 const store = configureStore()
 
+
 const RNRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <AntProvider>
+      <App />
+
+    </AntProvider>
+  </Provider>
+
 )
 
 AppRegistry.registerComponent(appName, () => RNRedux);
